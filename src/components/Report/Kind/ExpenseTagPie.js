00a67ba3aa39948fe9433787bdeedb943123f8c1
TiagoFFaria <tiagomourafaria2@gmail.com>
2019-12-01 23:46:33 -0300

    Fixing pie chart calcs

--- src/components/Report/Kind/ExpenseTagPie.js
+++ src/components/Report/Kind/ExpenseTagPie.js
@@ -2,22 +2,36 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ChartistGraph from 'react-chartist';
 import Chartist from 'chartist';
-import abbreviate from 'number-abbreviate';
 import 'chartist-plugin-tooltips';
 import 'chartist/dist/chartist.min.css';
 import 'chartist-plugin-tooltips/dist/chartist-plugin-tooltip.css';
 
 class ExpenseByTagsPie extends React.Component {
   render() {
-    const sum = function(a, b) { return a.value + b.value };
     const _self = this;
+    var total=0;
+    var size=0;
+    var pointer=0;
     const options = {
       labelInterpolationFnc: function(value , index) {
         const label = _self.props.data.series[index].name;
         if ( _self.props.data.series.length === 1 ) {
           return label + ' - 100%';
         }
-        return label + ' - ' + Math.round(value / _self.props.data.series.reduce(sum) * 100) + '%';
+        //Collecting total ammount of expenses
+        size = _self.props.data.series.length;
+        pointer = 0;
+        total = 0;
+        while(size>0){
+          total += _self.props.data.series[pointer].value;
+          console.log('total:'+total);
+          size--;
+          pointer++;
+        }
+        console.log('size after colection'+ size);
+        //End of colecction
+
+        return label + ' - ' + Math.round(value / total* 100) + '%';
       },
       labelOffset: 80,
       chartPadding: 20,
